306dbdd3b48c601491db55fd8aac0a194433310c
Michal Moravec <mishal@mishal.cz>
2016-08-26 15:39:19 +0200

    Updated travis tests - test using php-geos for php7

--- .travis/install_geos.sh
+++ .travis/install_geos.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+
 wget https://github.com/libgeos/libgeos/archive/$VERSION.tar.gz
 tar zxf $VERSION.tar.gz
 cd libgeos-$VERSION
 ./autogen.sh
-./configure --enable-php
+./configure
 make
-mv php/.libs/geos.so $(php-config --extension-dir)
-cd ..
+sudo make install
 
+git clone https://git.osgeo.org/gogs/geos/php-geos.git
+cd php-geos
+./autogen.sh
+./configure
+make # generates modules/geos.so
+mv modules/geos.so $(php-config --extension-dir)
+cd ..
 echo "extension=geos.so" > geos.ini
 phpenv config-add geos.ini
